792dee498ad3dae0999523c2710bbfb99e635cdf
Daniel Wallace <danielwallace@gtmanfred.com>
2016-11-17 16:37:53 -0600

    pytest fixture for requests

--- salt/test/integration/default/testinfra/test_nginx.py
+++ salt/test/integration/default/testinfra/test_nginx.py
@@ -1,6 +1,14 @@
 import os
+import pytest
 import requests
 import testinfra
+
+
+@pytest.fixture()
+def Request():
+    def f(arg):
+        return requests.get(arg)
+    return f
 
 
 def test_service_is_running_and_enabled(Service):
@@ -9,6 +17,6 @@ def test_service_is_running_and_enabled(Service):
     assert nginx.is_enabled
 
 
-def test_request_blog(Interface):
+def test_request_blog(Request):
     link = 'http://{0}:8000/'.format(os.environ.get('DOCKER_IP', 'localhost'))
-    assert 'gtmanfred' in requests.get(link).content
+    assert 'gtmanfred' in Request(link).content
